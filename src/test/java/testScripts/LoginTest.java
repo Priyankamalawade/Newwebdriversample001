7a9dd2380c7819dbb934cf49d4e3e9872fd42ee4
Administrator <priyankamalawade1999@gmail.com>
2023-08-09 07:14:03 +0000

    Login test updated

--- src/test/java/testScripts/LoginTest.java
+++ src/test/java/testScripts/LoginTest.java
@@ -1,8 +1,15 @@
 package testScripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
 
 	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/login");
+		
+		
 		
 
 	}
