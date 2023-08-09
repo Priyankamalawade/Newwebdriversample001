2757ddf7384e2aa8de20af660eb9af14aa4cc5d1
Administrator <priyankamalawade1999@gmail.com>
2023-08-09 08:09:26 +0000

    new login test

--- src/test/java/testScripts/LoginTest.java
+++ src/test/java/testScripts/LoginTest.java
@@ -1,5 +1,6 @@
 package testScripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,7 +8,11 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://the-internet.herokuapp.com/login");
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.className("radius")).click();
 		
 		
 		
