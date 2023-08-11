c2a513ed18dbdaca84a7a35110ac29ffd0decb58
Administrator <Administrator@172.31.7.68>
2023-08-11 09:10:43 +0000

    webtable

--- src/test/java/testScripts/WebTable.java
+++ src/test/java/testScripts/WebTable.java
@@ -2,7 +2,9 @@ package testScripts;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
 
 public class WebTable {
 
@@ -12,8 +14,8 @@ public class WebTable {
 		driver.get("http://demo.seleniumeasy.com/table-sort-search-demo.html");
 		String strSalary = driver.findElement(By.xpath("//td[contains(text(), 'Wagner' )]//following-sibling::td[5]")).getText();
 		System.out.println(strSalary);
-		//string position = driver.findElement(By.tagName("td")).toRightof(By.xpath("//td[contains(text(), 'Wagner'"))
-		
+		String position = driver.findElement(with(By.tagName("td")).toRightOf(By.xpath("//td[contains(text(), 'Wagner'"))).getText();
+		System.out.println("position..." + position);
 
 	}
 
